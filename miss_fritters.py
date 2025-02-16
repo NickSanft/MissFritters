d6c355ee7be07481aebf00be81920fdaaf9be0bf
Nick Sanft <nsanft@sanft.com>
2025-02-15 21:45:20 -0600

    Checkpointer can be persistent, store

--- miss_fritters.py
+++ miss_fritters.py
@@ -1,14 +1,20 @@
 import glob
+import uuid
 
+from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
 import re
+from contextlib import ExitStack
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.store.memory import InMemoryStore
 
 from message_source import MessageSource
 from tools import get_weather, deck_reload, deck_draw_cards, deck_cards_left, roll_dice, search_web, get_current_time, \
     tell_a_story, describe_image
 
+PERSISTENT = True
 LLAMA_MODEL = "llama3.2"
 IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff"]
 
@@ -22,23 +28,9 @@ Role:
 
     Here are the tools available to you, do not call these tools unless you need to.
 
-    get_current_time: returns the current time as an RFC3339 timestamp in US / Central Standard Time.
+    search_memories: can be provided a query to print a list of memories for a given user.
     
-    describe_image: Only use this if a file path is provided. Used to describe an image. The filepaths you have are: {files}
-
-    tell_a_story: Only use this if the user asks to tell a story.
-
-    search_web: Searches the internet for a term and returns the results. Use this if you are unsure what the user is asking.
-
-    roll_dice: used to roll different types of dice.
-
-    get_weather: used to get the temperature from a specific city in Fahrenheit. Please do not use it unless the user gives you a specific city.'
-
-    deck_draw_cards: Used to draw cards from a deck of cards.
-
-    deck_cards_left: Used to find the remaining cards in a deck of cards.
-
-    deck_reload: Shuffles or reloads the deck of cards that is currently active for the user.
+    add_memory: can be used to add a memory if requested by the user.
 """
 
 
@@ -83,7 +75,7 @@ def ask_stuff(base_prompt: str, source: MessageSource, user_id: str) -> str:
     print(f"Role description: {full_system_desc}")
     print(f"Prompt to ask: {full_prompt}")
 
-    config = {"configurable": {"thread_id": user_id_clean}}
+    config = {"configurable": {"user_id": user_id_clean, "thread_id": user_id_clean}}
     inputs = {"messages": [("system", full_system_desc), ("user", full_prompt)]}
     ollama_response = print_stream(graph.stream(inputs, config=config, stream_mode="values"))
 
@@ -102,11 +94,50 @@ def print_stream(stream):
     return message.content
 
 
-tools = [tell_a_story, get_weather, roll_dice, deck_reload, deck_draw_cards, deck_cards_left, search_web,
-         get_current_time, describe_image]
+@tool(parse_docstring=True)
+def search_memories(user_id: str, search_query: str):
+    """ This function searches for memories made from previous conversations. Return a dict of memories.
 
+    Args:
+        user_id (str): The id of the user to search the memory for.
+        search_query (str): The query to search memories of previous conversations with the user.
+    """
+    search_result = store.search(
+        (user_id, "memories"),
+        query=search_query,
+        limit=3  # Return top 3 matches
+    )
+    print(f"Search result: {search_result}")
+    return str(search_result)
+
+
+@tool(parse_docstring=True)
+def add_memory(user_id: str, memory_key: str, memory_to_store: str):
+    """ This function stores a memory. Only use this if the user has asked you to.
+
+    Args:
+        user_id (str): The id of the user to store the memory.
+        memory_key (str): A unique identifier for the memory.
+        memory_to_store (str): The memory you wish to store.
+    """
+    memory_dict = {memory_key: memory_to_store}
+    store.put((user_id, "memories"), str(uuid.uuid4()), memory_dict)
+    return "Added memory for {}: {}".format(memory_key, memory_to_store)
+
+
+# tools = [tell_a_story, get_weather, roll_dice, deck_reload, deck_draw_cards, deck_cards_left, search_web, get_current_time, describe_image]
+tools = [search_memories, add_memory]
+
+if PERSISTENT:
+    db_name = "chat_history.db"
+else:
+    db_name = ":memory:"
+
+store = InMemoryStore()
+exit_stack = ExitStack()
+checkpointer = exit_stack.enter_context(SqliteSaver.from_conn_string(db_name))
 ollama_instance = ChatOllama(model=LLAMA_MODEL)
-graph = create_react_agent(ollama_instance, tools=tools, checkpointer=memory)
+graph = create_react_agent(ollama_instance, tools=tools, checkpointer=checkpointer, store=store)
 
 # with open("test.png", "wb") as binary_file:
 #     binary_file.write(graph.get_graph().draw_mermaid_png())
